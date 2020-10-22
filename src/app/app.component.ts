a4f0a1316348684f5cf1bea87f7d30de9162fe8f
Adam <adam@cledar.pl>
2020-10-22 09:26:28 +0200

    2-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-eduweb-app';
+
+  person = {
+    name: 'Testowy',
+    birthdate: 1997,
+    company: {
+      name: 'Eduweb'
+    }
+  };
+
+  get getAge(): number {
+    const currentYear: string = moment().format('Y');
+    return Number(currentYear) - this.person.birthdate;
+  }
 }
